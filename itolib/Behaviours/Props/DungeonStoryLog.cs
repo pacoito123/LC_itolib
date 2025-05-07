38f16e14729167a6c278d2f4f0797e9a8ab6704c
pacoito123 <pacoito@protonmail.com>
2025-05-07 03:06:45 -0600

    Added DungeonStoryLog for interior story logs. - Basically the same as a regular story log, but if it happens to be an ExtendedStoryLog, it compares the current dungeon's name instead of the level's name when replacing its ID number.

---
+++ itolib/Behaviours/Props/DungeonStoryLog.cs
@@ -0,0 +1,53 @@
+using LethalLevelLoader;
+using UnityEngine.Events;
+
+namespace itolib.Behaviours.Props
+{
+    /// <summary>
+    /// 	TODO.
+    /// </summary>
+    public class DungeonStoryLog : StoryLog
+    {
+        /// <summary>
+        ///     TODO.
+        /// </summary>
+        public UnityEvent<int>? onLogSpawned;
+
+        /// <summary>
+        ///     TODO.
+        /// </summary>
+        public UnityEvent<int>? onAlreadyUnlocked;
+
+        /// <summary>
+        ///     TODO.
+        /// </summary>
+        public new void Start()
+        {
+            if (DungeonManager.CurrentExtendedDungeonFlow == null)
+            {
+                return;
+            }
+
+            foreach (ExtendedStoryLog extendedStoryLog in DungeonManager.CurrentExtendedDungeonFlow.ExtendedMod.ExtendedStoryLogs)
+            {
+                if (string.CompareOrdinal(extendedStoryLog.sceneName, DungeonManager.CurrentExtendedDungeonFlow.DungeonName) == 0
+                    && storyLogID == extendedStoryLog.storyLogID)
+                {
+                    // Publicized LLL for access to 'ExtendedStoryLog.newStoryLogID' specifically...
+                    if (!TerminalManager.Terminal.unlockedStoryLogs.Contains(extendedStoryLog.newStoryLogID))
+                    {
+                        onLogSpawned?.Invoke(extendedStoryLog.newStoryLogID);
+                        storyLogID = extendedStoryLog.newStoryLogID;
+                    }
+                    else
+                    {
+                        onAlreadyUnlocked?.Invoke(extendedStoryLog.newStoryLogID);
+                        RemoveLogCollectible();
+                    }
+
+                    break;
+                }
+            }
+        }
+    }
+}
