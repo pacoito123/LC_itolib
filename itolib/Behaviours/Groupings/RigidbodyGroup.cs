f84f598eb6132b3d5bf8207419736d1b9da2ef0f
pacoito123 <pacoito@protonmail.com>
2026-01-27 17:44:20 -0600

    Added some stuff to RigidbodyGroup. - Added methods to apply force relative to each Rigidbody's position. - Added methods to wake up and sleep every Rigidbody.

--- itolib/Behaviours/Groupings/RigidbodyGroup.cs
+++ itolib/Behaviours/Groupings/RigidbodyGroup.cs
@@ -19,10 +19,28 @@ namespace itolib.Behaviours.Groupings
         /// <summary>
         ///     TODO.
         /// </summary>
+        /// <param name="acceleration"></param>
+        public void ApplyRelativeContinuousAcceleration(Vector3 acceleration)
+        {
+            PerformGroupAction(rigidbody => rigidbody.AddRelativeForce(acceleration, ForceMode.Acceleration));
+        }
+
+        /// <summary>
+        ///     TODO.
+        /// </summary>
         /// <param name="force"></param>
         public void ApplyContinuousForce(Vector3 force)
         {
             PerformGroupAction(rigidbody => rigidbody.AddForce(force, ForceMode.Force));
+        }
+
+        /// <summary>
+        ///     TODO.
+        /// </summary>
+        /// <param name="force"></param>
+        public void ApplyRelativeContinuousForce(Vector3 force)
+        {
+            PerformGroupAction(rigidbody => rigidbody.AddRelativeForce(force, ForceMode.Force));
         }
 
         /// <summary>
@@ -37,10 +55,56 @@ namespace itolib.Behaviours.Groupings
         /// <summary>
         ///     TODO.
         /// </summary>
+        /// <param name="impulse"></param>
+        public void ApplyRelativeInstantImpulse(Vector3 impulse)
+        {
+            PerformGroupAction(rigidbody => rigidbody.AddRelativeForce(impulse, ForceMode.Impulse));
+        }
+
+        /// <summary>
+        ///     TODO.
+        /// </summary>
         /// <param name="velocityChange"></param>
         public void ApplyInstantVelocityChange(Vector3 velocityChange)
         {
             PerformGroupAction(rigidbody => rigidbody.AddForce(velocityChange, ForceMode.VelocityChange));
+        }
+
+        /// <summary>
+        ///     TODO.
+        /// </summary>
+        /// <param name="velocityChange"></param>
+        public void ApplyRelativeInstantVelocityChange(Vector3 velocityChange)
+        {
+            PerformGroupAction(rigidbody => rigidbody.AddRelativeForce(velocityChange, ForceMode.VelocityChange));
+        }
+
+        /// <summary>
+        ///     TODO.
+        /// </summary>
+        public void SleepAll()
+        {
+            PerformGroupAction(rigidbody =>
+            {
+                if (!rigidbody.IsSleeping())
+                {
+                    rigidbody.Sleep();
+                }
+            });
+        }
+
+        /// <summary>
+        ///     TODO.
+        /// </summary>
+        public void WakeUpAll()
+        {
+            PerformGroupAction(rigidbody =>
+            {
+                if (rigidbody.IsSleeping())
+                {
+                    rigidbody.WakeUp();
+                }
+            });
         }
     }
 }
